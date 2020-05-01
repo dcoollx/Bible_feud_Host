64ebcfb599fa9a87ea6e495527b56217b47efde4
David Queen <dsqjr@msn.com>
2020-05-01 19:42:27 -0400

    added react

---
+++ bible_feud/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        
+      </header>
+      <main>
+    <h1>Bible Feud</h1>
+    <div>
+      <button>Host a game</button>
+      <button>Join A game</button>
+    </div>
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
